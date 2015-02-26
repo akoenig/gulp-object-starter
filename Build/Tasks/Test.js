f3d00493cacef24d9fcb06e30974fd5a4e20f974
Tyll Weiß <inkdpixels@googlemail.com>
2015-02-26 21:09:17 +0100

    [TASK] Use a single instance of jshint for all script sources for performance reasons

--- Build/Tasks/Test.js
+++ Build/Tasks/Test.js
@@ -3,6 +3,8 @@ var jshint = require('gulp-jshint');
 var config = require('./../Config');
 
 gulp.task('test', function() {
+    var scriptPaths = [];
+
     config.packages.forEach(function(packageConfig) {
         var jsConfig = packageConfig.scripts;
         var bundles = jsConfig.bundles;
@@ -11,8 +13,10 @@ gulp.task('test', function() {
             return this;
         }
 
-        return gulp.src(packageConfig.basePath + jsConfig.src + '*.js')
-            .pipe(jshint())
-            .pipe(jshint.reporter('jshint-stylish'));
+        scriptPaths.push(packageConfig.basePath + jsConfig.src + '*.js');
     });
+
+    return gulp.src(scriptPaths)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
 });
