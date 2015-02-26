e1a9655301938b98fa562f1a19b16d322398381b
Tyll Weiß <inkdpixels@googlemail.com>
2015-02-26 21:07:28 +0100

    [BUGFIX] Fix an old reference to the extension array in the config

--- Build/Tasks/Test.js
+++ Build/Tasks/Test.js
@@ -3,15 +3,15 @@ var jshint = require('gulp-jshint');
 var config = require('./../Config');
 
 gulp.task('test', function() {
-    config.extensions.forEach(function(extensionConfig) {
-        var jsConfig = extensionConfig.scripts;
+    config.packages.forEach(function(packageConfig) {
+        var jsConfig = packageConfig.scripts;
         var bundles = jsConfig.bundles;
 
         if(!jsConfig || !bundles) {
             return this;
         }
 
-        return gulp.src(extensionConfig.basePath + jsConfig.src + '*.js')
+        return gulp.src(packageConfig.basePath + jsConfig.src + '*.js')
             .pipe(jshint())
             .pipe(jshint.reporter('jshint-stylish'));
     });
