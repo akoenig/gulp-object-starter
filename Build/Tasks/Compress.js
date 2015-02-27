5544f57adb94bca6107fb23915deade32f14a2c6
Tyll Weiß <inkdpixels@googlemail.com>
2015-02-27 09:43:56 +0100

    [FEATURE] Add a css minifier to the 'compile' task

--- Build/Tasks/Compress.js
+++ Build/Tasks/Compress.js
@@ -1,18 +1,24 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
 var config = require('./../Config');
 
 gulp.task('compress', function() {
     config.packages.forEach(function(packageConfig) {
-        var scriptsConfig = packageConfig.scripts;
         var packageBasePath = packageConfig.basePath;
+        var scriptsConfig = packageConfig.scripts;
+        var sassConfig = packageConfig.sass;
 
-        if(!scriptsConfig) {
-            return;
+        if(scriptsConfig) {
+            gulp.src(packageBasePath + scriptsConfig.dest + '/**/*.js')
+                .pipe(uglify())
+                .pipe(gulp.dest(packageBasePath + scriptsConfig.dest));
         }
 
-        return gulp.src(packageBasePath + scriptsConfig.dest + '/**/*.js')
-            .pipe(uglify())
-            .pipe(gulp.dest(packageBasePath + scriptsConfig.dest));
+        if(sassConfig) {
+            gulp.src(packageBasePath + sassConfig.dest + '/**/*.css')
+                .pipe(minifyCSS())
+                .pipe(gulp.dest(packageBasePath + sassConfig.dest));
+        }
     });
 });
