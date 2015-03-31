fd2e6b88e01781bb3c47a5cfd5c3351898fe6a41
Tyll Weiß <weiss@sitegeist.de>
2015-03-31 14:09:58 +0200

    [TASK] Add the packageConfigs in the Gulpfile, and wrap the Build Constructor into a singleton export

--- Build/Index.js
+++ Build/Index.js
@@ -1,25 +1,51 @@
+var _ = require('lodash');
 var Packages = require('./Repositories/Packages.js');
 var createParentTasks = require('./Utilities/CreateParentTasks.js');
-var packagesRepository = new Packages();
-var tasksArray;
+var singleton;
 
-// Setup tasks which are re-created for each package.
-packagesRepository.addTask([
-	require('./Tasks/Compilers/Sass.js'),
-	require('./Tasks/Compilers/Scripts.js'),
-	require('./Tasks/Compilers/Images.js')
-]);
+var Starter = function() {
+	'use strict';
 
-// Setup the model, which will create it's tasks, based on the .addTask() method above.
-packagesRepository.addPackage(require('./../Web/typo3conf/ext/examplePackage/BuildConfig.js'));
+	this.packagesRepository = new Packages();
 
-// Setup general tasks which aren't depending on a single package.
-tasksArray = require('./Config.js').tasks;
+	this.createPackageBasedTasks();
+};
 
-// Create a task with the second argument, which runs every task which name inherits the second argument as a part of the name. F.e.
-// createParentTasks(tasksArray, 'compile:sass'); -> gulp.task -> ['compile:sass:examplePackage1', 'compile:sass:examplePackage2']
-createParentTasks(tasksArray, 'compile:sass');
-createParentTasks(tasksArray, 'compile:images');
-createParentTasks(tasksArray, 'compile:scripts');
+Starter.prototype.createPackageBasedTasks = function() {
+	'use strict';
 
-module.exports = packagesRepository;
+	// Setup tasks which are re-created for each package.
+	this.packagesRepository.addTask([
+		require('./Tasks/Compilers/Sass.js'),
+		require('./Tasks/Compilers/Scripts.js'),
+		require('./Tasks/Compilers/Images.js')
+	]);
+};
+
+Starter.prototype.addPackages = function(packageConfigs) {
+	'use strict';
+
+	_.forEach(packageConfigs, function(packageConfig) {
+		this.packagesRepository.addPackage(packageConfig);
+	}.bind(this));
+
+	this.createParentTasks();
+};
+
+Starter.prototype.createParentTasks = function() {
+	'use strict';
+
+	var tasksArray = require('./Config.js').tasks;
+
+	// Create a task with the second argument, which runs every task which name inherits the second argument as a part of the name. F.e.
+	// createParentTasks(tasksArray, 'compile:sass'); -> gulp.task -> ['compile:sass:examplePackage1', 'compile:sass:examplePackage2']
+	createParentTasks(tasksArray, 'compile:sass');
+	createParentTasks(tasksArray, 'compile:images');
+	createParentTasks(tasksArray, 'compile:scripts');
+};
+
+module.exports = function() {
+	singleton = singleton || new Starter();
+
+	return singleton;
+}();
--- Gulpfile.js
+++ Gulpfile.js
@@ -1,1 +1,5 @@
-require('./Build/Index.js');
+var starter = require('./Build/Index.js');
+
+starter.addPackages([
+	require('./Web/typo3conf/ext/examplePackage/BuildConfig.js')
+])
