d7e14d2f67963734c2fa26a93754303fb71e9fff
Tyll Weiß <weiss@sitegeist.de>
2015-04-07 12:11:38 +0200

    [TASK] Abstract and document the createTasks method

--- Build/Models/Package.js
+++ Build/Models/Package.js
@@ -43,31 +43,43 @@ Package.prototype.createTasks = function() {
 	var options = this.options;
 	var tasks = this.repository.getPackageTasks();
 
+	// Loop over each registered task.
 	_.forEach(tasks, function(taskExport) {
-		var hasRequiredKeys = _.isObject(taskExport) && taskExport.requiredKeysObject;
+		var hasTaskRequiredKeys = _.isObject(taskExport) && taskExport.requiredKeysObject;
 		var taskFunction = taskExport;
 		var hasRequiredAttributes = true;
 
-		if(hasRequiredKeys) {
+		// If the task itself has a required object structure for the package, evaluate it.
+		if(hasTaskRequiredKeys) {
+			hasRequiredAttributes = packageModel.evaluteTasksRequiredKeys(taskExport.requiredKeysObject, options);
+
+			// Re-Assign the variable which will be executed if the taskExport is an object.
 			taskFunction = taskExport.createTask;
-
-			_.forEach(taskExport.requiredKeysObject, function(requiredKeys, targetKey) {
-				var testResults = hasObjectRequiredKeys(options[targetKey], requiredKeys);
-
-				if(!testResults.result && testResults.missingKey && hasRequiredAttributes) {
-					hasRequiredAttributes = false;
-
-					throw new Error('Missing attribute "' + testResults.missingKey + '" in object "' + targetKey + '" ' + errorMessageSuffix);
-				}
-			});
 		}
 
+		// If all attributes are present, create the task.
 		if(hasRequiredAttributes) {
 			taskFunction(packageModel);
 		}
 	});
 
 	return this;
+};
+
+Package.prototype.evaluteTasksRequiredKeys = function evaluteTasksRequiredKeys(tasksRequiredKeysObject, packageOptions) {
+	var hasRequiredAttributes = true;
+
+	_.forEach(tasksRequiredKeysObject, function(requiredKeys, targetKey) {
+		var testResults = hasObjectRequiredKeys(packageOptions[targetKey], requiredKeys);
+
+		if(!testResults.result && testResults.missingKey && hasRequiredAttributes) {
+			hasRequiredAttributes = false;
+
+			throw new Error('Missing attribute "' + testResults.missingKey + '" in object "' + targetKey + '" ' + errorMessageSuffix);
+		}
+	});
+
+	return hasRequiredAttributes;
 };
 
 module.exports = Package;
